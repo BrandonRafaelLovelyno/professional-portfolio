cfd77b266929483d48d6dd9cfc31e740b664b5de
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-01-09 11:40:16 +0700

    feat : continuing feature page

--- components/display/coding-project-page/feature-section.tsx
+++ components/display/coding-project-page/feature-section.tsx
@@ -1,27 +1,38 @@
 import { Feature } from "@/data/coding-project-detail-data";
 import React, { useState } from "react";
+import SingleFeature from "./single-feature";
 
 interface FeatureSectionProps {
   features: Feature[];
 }
 
 const FeatureSection: React.FC<FeatureSectionProps> = ({ features }) => {
-  const [featIndex, setFeatIndex] = useState(0);
+  const [index, setindex] = useState(1);
   return (
     <div className="flex flex-row w-[80%] h-full">
-      <div className="w-[90%] h-full bg-red-500">{featIndex}</div>
-      <div className="flex-1 bg-background flex flex-col justify-center items-center">
-        {features.map((feat, index) => (
+      <div className="w-[90%] h-full  flex flex-col overflow-hidden relative">
+        {features.map((feat, featIndex) => (
+          <SingleFeature
+            feature={feat}
+            currentIndex={index}
+            featureIndex={featIndex + 1}
+            key={featIndex}
+          />
+        ))}
+      </div>
+      <div className="flex-1 bg-background flex flex-col justify-center items-center gap-y-5">
+        {features.map((feat, featIndex) => (
           <button
-            className="p-4 border-2 rounded-full border-secondary hover:border-primary hover:bg-primary hover:text-background transition-all duration-300 font-semibold relative"
+            className="p-6 border-2 rounded-full border-secondary hover:border-primary hover:bg-primary hover:text-background transition-all duration-300 font-semibold flex justify-center items-center"
             onClick={() => {
-              setFeatIndex(index);
+              setindex(featIndex);
             }}
           >
-            p
-            <span className="absolute top-1/2 left-1/2 -translate-y-1/2">
-              {index}
-            </span>
+            <div className="w-0 h-0 relative">
+              <span className="absolute -translate-x-1/2 -translate-y-1/2">
+                {featIndex + 1}
+              </span>
+            </div>
           </button>
         ))}
       </div>
---
+++ components/display/coding-project-page/single-feature.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { Feature } from "@/data/coding-project-detail-data";
+import Image from "next/image";
+
+interface SingleFeatureProps {
+  currentIndex: number;
+  featureIndex: number;
+  feature: Feature;
+}
+
+function findVariant(currentIndex: number, featureIndex: number): string {
+  if (currentIndex == featureIndex) {
+    return "onView";
+  }
+  if (currentIndex > featureIndex) {
+    return "onNext";
+  }
+  return "onPrev";
+}
+
+const SingleFeature: React.FC<SingleFeatureProps> = ({
+  currentIndex,
+  featureIndex,
+  feature,
+}) => {
+  return (
+    <motion.div
+      className={twMerge(
+        "w-full h-full absolute",
+
+        "flex flex-col py-9 pl-9"
+      )}
+      variants={{
+        onPrev: { y: "100%" },
+        onView: { y: 0 },
+        onNext: { y: "-100%" },
+      }}
+      transition={{ type: "spring", stiffness: 60 }}
+      initial={findVariant(currentIndex, featureIndex)}
+      animate={findVariant(currentIndex, featureIndex)}
+    >
+      <div className="flex flex-row gap-x-5 items-center">
+        <div className="p-8 border-4 border-white rounded-full flex justify-center items-center">
+          <div className="w-0 h-0 relative">
+            <span className="-translate-x-1/2 text-5xl font-bold -translate-y-1/2 absolute">
+              {featureIndex}
+            </span>
+          </div>
+        </div>
+        <span className="text-5xl font-bold">{feature.title}</span>
+      </div>
+      <div className="mt-10 flex flex-row w-full justify-center gap-x-10 items-center">
+        <div className="w-[576px] h-[324px] bg-primary"></div>
+        <div className="w-[189px] h-[336px] bg-primary"></div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default SingleFeature;

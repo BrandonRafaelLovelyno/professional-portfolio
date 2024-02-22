f05cb7f27e2a144fab0e71466e74aabac79faaa2
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-02-22 16:05:33 +0700

    feat : coding experience assets

--- data/coding-event-section-data.ts
+++ data/coding-event-section-data.ts
@@ -3,19 +3,7 @@ import { Event, Experience } from "./org-exp-section-data";
 const FULLSTACK_EV: Event[] = [
   {
     date: "September 2023 - Present",
-    img: "https://plus.unsplash.com/premium_photo-1670044020170-5ba458728767?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "NESCO UGM 2023",
-    position: "Staff",
-    eventDesc:
-      "National Electrical Power System Competition is a national contest focusing on high-voltage electricity, aimed at improving Indonesia's power sector. Through innovation and knowledge sharing, the competition strives to enhance skills and contribute to the development of a more reliable and efficient electrical grid for the nation's future.",
-    eventRole: [
-      "Specializing in React for dynamic user interface development.",
-      "Proficient in server-side logic using Node.js with Express",
-    ],
-  },
-  {
-    date: "September 2023 - Present",
-    img: "https://plus.unsplash.com/premium_photo-1670044020170-5ba458728767?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "/coding experience/findit.jpg",
     name: "Find IT! UGM 2023",
     position: "Staff",
     eventDesc:
@@ -27,7 +15,7 @@ const FULLSTACK_EV: Event[] = [
   },
   {
     date: "October 2023 - November 2023",
-    img: "https://plus.unsplash.com/premium_photo-1670044020170-5ba458728767?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "/coding experience/kpu.jpg",
     name: "General Elections Commisions of Engineering Faculty",
     position: "Staff",
     eventDesc:
@@ -41,10 +29,30 @@ const FULLSTACK_EV: Event[] = [
   },
 ];
 
+const BACKEND_EV: Event[] = [
+  {
+    date: "September 2023 - Present",
+    img: "/coding experience/nesco.jpg",
+    name: "NESCO UGM 2023",
+    position: "Staff",
+    eventDesc:
+      "National Electrical Power System Competition is a national contest focusing on high-voltage electricity, aimed at improving Indonesia's power sector. Through innovation and knowledge sharing, the competition strives to enhance skills and contribute to the development of a more reliable and efficient electrical grid for the nation's future.",
+    eventRole: [
+      "Working with Typescript for CRUD action",
+      "Specializing on server-side logic using Node.js with Express",
+      "Contributing in server-side authentication using Node.js with Express",
+    ],
+  },
+];
+
 const CODING_EXP: Experience[] = [
   {
     events: FULLSTACK_EV,
     position: "Full-stack Developer",
+  },
+  {
+    events: BACKEND_EV,
+    position: "Back-end Developer",
   },
 ];
 

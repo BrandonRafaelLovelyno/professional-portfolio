dd44c3c1901ddf1f6611d123badd128daa729d90
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-03-07 18:16:39 +0700

    feat : exp-detail responsive mobile

--- components/display/non-coding-project-page/exp-detail.tsx
+++ components/display/non-coding-project-page/exp-detail.tsx
@@ -17,23 +17,31 @@ const ExpDetail: React.FC<ExpDetailProps> = ({ onClose, event }) => {
       animate={isClosing ? { opacity: 0 } : { opacity: 1 }}
       initial={{ opacity: 0 }}
       transition={{ duration: 0.5, delay: 0.5 }}
-      className="w-full h-full bg-background bg-opacity-70 z-10 absolute flex flex-col py-20 gap-x-8 text-3xl px-20 justify-center gap-y-10"
+      className="w-full h-full bg-background bg-opacity-70 z-10 absolute flex flex-col py-20 gap-x-8 lg:px-20 px-5 justify-center gap-y-16"
     >
       <Reveal childrenDir="up" hide={isClosing} customDelay={0.5}>
-        <div className="flex flex-col gap-y-5">
-          <p className="text-secondary italic">What is {event.name}?</p>
-          <p className="text-sm">{event.eventDesc}</p>
+        <div className="flex flex-col gap-y-8 text-3xl">
+          <p className="text-primary max-lg:text-center italic">
+            What is {event.name}?
+          </p>
+          <p className="text-sm text-neutral-300 font-thin tracking-widest max-lg:text-center max-lg:line-clamp-4">
+            {event.eventDesc}
+          </p>
         </div>
       </Reveal>
       {event.eventRole && (
         <Reveal childrenDir="up" hide={isClosing} customDelay={0.5}>
-          <div className="flexf flex-col gap-y-5 text-xl">
-            <p className=" text-secondary italic">What I Do?</p>
+          <div className="flex   flex-col gap-y-8 text-xl">
+            <p className=" text-secondary max-lg:text-center italic">
+              What I Do?
+            </p>
             <div className="flex flex-col gap-y-2">
               {event.eventRole.map((role) => (
                 <div key={role} className="flex flex-row gap-x-2 items-center">
                   <VscDebugBreakpointData size={10} />
-                  <p className="text-sm">{role}</p>
+                  <p className="text-sm tracking-widest text-neutral-300 max-lg:line-clamp-2">
+                    {role}
+                  </p>
                 </div>
               ))}
             </div>

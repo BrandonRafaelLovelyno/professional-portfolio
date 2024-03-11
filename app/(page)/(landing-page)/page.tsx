b44c0fa86a89db71ca220bf6421c20ce74a7389a
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-03-11 13:34:05 +0700

    feat : landingPage - client experience assets

--- app/(page)/(landing-page)/page.tsx
+++ app/(page)/(landing-page)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { Parallax } from "@react-spring/parallax";
 import LandingLayer from "@/components/display/landing-page/landing-section";
 import ReusableLayer from "@/components/display/reusable-section";
 import { twMerge } from "tailwind-merge";
--- data/home-section-data.ts
+++ data/home-section-data.ts
@@ -19,6 +19,54 @@ export const HEROTITLE: string[] = [
 
 export const HOMESECTIONDATA: Section[] = [
   {
+    description:
+      "I've undertaken ambitious coding projects in becoming a full-stack developer. These projects reflect my dedication to mastering diverse aspects of web development",
+    firstWord: "Coding",
+    secondWord: "Project",
+    position: ["Discord", "Spotify", "Twitter", "more"],
+    firstIcon: "/coding project/parallax/twitter.png",
+    secondIcon: "/coding project/parallax/discord.png",
+    thirdIcon: "/coding project/parallax/project.png",
+    fourthIcon: "/coding project/parallax/spotify.png",
+    link: "/coding-pro",
+  },
+  {
+    description:
+      "This collection serves as a testament to my effort in positioning myself to venture into the realm of website freelancing with confidence and expertise",
+    firstWord: "Client",
+    secondWord: "Experience",
+    position: ["Ez Parenting"],
+    link: "/client-exp",
+    firstIcon: "/client experience/parallax/client.png",
+    secondIcon: "/client experience/parallax/product.png",
+    thirdIcon: "/client experience/parallax/programmer.png",
+    fourthIcon: "/client experience/parallax/software.png",
+  },
+  {
+    description:
+      "I've contributed in building websites for a number of events. These experiences reflect my ability and dedication to build a website as a team",
+    firstWord: "Coding",
+    secondWord: "Event",
+    position: ["Find IT! UGM", "NESCO UGM", "KPU FT UGM"],
+    link: "/coding-ev",
+    firstIcon: "/coding experience/parallax/code.png",
+    secondIcon: "/coding experience/parallax/api.png",
+    thirdIcon: "/coding experience/parallax/database.png",
+    fourthIcon: "/coding experience/parallax/developer.png",
+  },
+  {
+    description:
+      "These accolades collectively emphasize my commitment to excellence, showcasing a versatile skill set that spans both technical and language domains.",
+    firstWord: "Certificate",
+    secondWord: "& Awards",
+    position: ["Tanoto Scholars", "Web Programming", "TOIEC English Test"],
+    firstIcon: "/awards/parallax/trophy.png",
+    secondIcon: "/awards/parallax/medal.png",
+    thirdIcon: "/awards/parallax/achievement.png",
+    fourthIcon: "/awards/parallax/winner.png",
+    link: "/certif",
+  },
+  {
     firstWord: "Work",
     secondWord: "Experience",
     firstIcon: "/work experience/parallax/briefcase.png",
@@ -46,54 +94,5 @@ export const HOMESECTIONDATA: Section[] = [
     thirdIcon: "/organizational experience/parallax/team.png",
     fourthIcon: "/organizational experience/parallax/organization.png",
     link: "/org-exp",
-  },
-  {
-    description:
-      "This collection serves as a testament to my effort in positioning myself to venture into the realm of website freelancing with confidence and expertise",
-    firstWord: "Client",
-    secondWord: "Experience",
-    position: ["Ez Parenting"],
-    link: "/client-exp",
-    firstIcon: "/coding experience/parallax/code.png",
-    secondIcon: "/coding experience/parallax/api.png",
-    thirdIcon: "/coding experience/parallax/database.png",
-    fourthIcon: "/coding experience/parallax/developer.png",
-  },
-  {
-    description:
-      "I've contributed in building websites for a number of events. These experiences reflect my ability and dedication to build a website as a team",
-    firstWord: "Coding",
-    secondWord: "Event",
-    position: ["Find IT! UGM", "NESCO UGM", "KPU FT UGM"],
-    link: "/coding-ev",
-    firstIcon: "/coding experience/parallax/code.png",
-    secondIcon: "/coding experience/parallax/api.png",
-    thirdIcon: "/coding experience/parallax/database.png",
-    fourthIcon: "/coding experience/parallax/developer.png",
-  },
-
-  {
-    description:
-      "I've undertaken ambitious coding projects in becoming a full-stack developer. These projects reflect my dedication to mastering diverse aspects of web development",
-    firstWord: "Coding",
-    secondWord: "Project",
-    position: ["Discord", "Spotify", "Twitter", "more"],
-    firstIcon: "/coding project/parallax/twitter.png",
-    secondIcon: "/coding project/parallax/discord.png",
-    thirdIcon: "/coding project/parallax/project.png",
-    fourthIcon: "/coding project/parallax/spotify.png",
-    link: "/coding-pro",
-  },
-  {
-    description:
-      "These accolades collectively emphasize my commitment to excellence, showcasing a versatile skill set that spans both technical and language domains.",
-    firstWord: "Certificate",
-    secondWord: "& Awards",
-    position: ["Tanoto Scholars", "Web Programming", "TOIEC English Test"],
-    firstIcon: "/awards/parallax/trophy.png",
-    secondIcon: "/awards/parallax/medal.png",
-    thirdIcon: "/awards/parallax/achievement.png",
-    fourthIcon: "/awards/parallax/winner.png",
-    link: "/certif",
   },
 ];

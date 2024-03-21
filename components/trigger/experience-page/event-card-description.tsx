c4d42400bf25351b3f4c7b82f662ade139081382
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-03-21 16:41:38 +0700

    feat : {experience Page } - read more

--- components/trigger/experience-page/event-card-description.tsx
+++ components/trigger/experience-page/event-card-description.tsx
@@ -17,11 +17,35 @@ const EventCardDescription: React.FC<EventCardDescriptionProps> = ({
         className={twMerge(
           "bottom-0 left-0 right-0",
           "card-description",
-          "flex items-center justify-center"
+          "flex flex-col justify-center px-5 rounded-lg bg-opacity-50",
+          "gap-y-3"
         )}
       >
-        <p className={twMerge("font-bold text-white text-2xl")}>
-          {experience.position}
+        <div className="flex flex-col gap-y-1">
+          <p
+            className={twMerge(
+              "font-extrabold font-Montserrat text-black text-md text-left whitespace-nowrap text-ellipsis overflow-hidden",
+              "max-lg:text-md"
+            )}
+          >
+            {experience.position}
+          </p>
+          <p
+            className={twMerge(
+              "font-bold text-stone-600 text-sm whitespace-nowrap text-ellipsis overflow-hidden",
+              "max-lg:text-xs"
+            )}
+          >
+            {event.data.eventName}
+          </p>
+        </div>
+        <p
+          className={twMerge(
+            "ml-auto text-lime-500 text-sm cursor-pointer",
+            "hover:underline hover:font-extrabold transition-all duration-300"
+          )}
+        >
+          Read more
         </p>
       </div>
     </>

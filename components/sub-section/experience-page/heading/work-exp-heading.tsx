accb682aad9b0dc25437baa423d696330d8ad588
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-05-03 12:54:43 +0700

    feat : event heading

--- components/sub-section/experience-page/heading/work-exp-heading.tsx
+++ components/sub-section/experience-page/heading/work-exp-heading.tsx
@@ -7,21 +7,23 @@ const WorkExpHeading = () => {
     <div
       className={twMerge(
         "w-full h-full pt-20",
-        "flex flex-row gap-y-5 items-start",
+        "flex flex-row gap-x-5 items-start",
         "px-6"
       )}
     >
       <div
-        className={twMerge(
-          "flex-1 w-fit",
-          "flex flex-row justify-end items-center"
-        )}
+        className={twMerge("flex-1", "flex flex-row justify-end items-center")}
       >
         <p className="text-3xl text-extrabold">I take video for education </p>
       </div>
-      <div
-        className={twMerge("flex-1 flex flex-row justify-start items-center")}
-      ></div>
+      <div className={twMerge("flex-1 flex flex-row justify-start")}>
+        <Image
+          src={"/image/work-exp/video-production/heading.jpg"}
+          width={100}
+          height={100}
+          alt="video-production"
+        />
+      </div>
     </div>
   );
 };

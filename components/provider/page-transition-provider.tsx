15fa7f65934555265698299fe111fd501f6cb1f5
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-08-12 09:50:56 +0700

    refactor : dark page transition

--- components/provider/page-transition-provider.tsx
+++ components/provider/page-transition-provider.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React, { createContext, useState } from "react";
 
 interface PageTransitionContext {
   isFading: boolean;
   setIsFading: (isFading: boolean) => void;
+  pushPage: (page: string) => void;
 }
 
 // Create the context
 export const PageTransitionContext = createContext<PageTransitionContext>({
   isFading: false,
   setIsFading: () => {},
+  pushPage: () => {},
 });
 
 export const PageTransitionProvider = ({
@@ -18,13 +21,22 @@ export const PageTransitionProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  const router = useRouter();
   const [isFading, setIsFading] = useState(false);
+
+  const pushPage = (page: string) => {
+    setIsFading(true);
+    setTimeout(() => {
+      router.push(page);
+    }, 1000);
+  };
 
   return (
     <PageTransitionContext.Provider
       value={{
         isFading,
         setIsFading,
+        pushPage,
       }}
     >
       {children}
